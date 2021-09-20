e39ab2da09fe6aad9348789b4a267525e3193887
Cagdas ISIK <cyasam@gmail.com>
2021-09-20 12:19:39 +0300

    More optimization

--- components/DayGridItem.js
+++ components/DayGridItem.js
@@ -1,9 +1,12 @@
 import Link from 'next/link';
+import dynamic from 'next/dynamic';
 import slugify from 'slugify';
 
 import { convertDayOfWeek } from '../util';
-import InfoWidget from './InfoWidget';
 import styles from '../styles/GridItem.module.css';
+
+// Load Components
+const InfoWidget = dynamic(() => import('./InfoWidget'));
 
 const DayGridItem = ({ item, location }) => {
   const dayOfWeek = convertDayOfWeek(item.date);
